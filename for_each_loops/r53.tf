eb960bdde55038893a99240abea92c54f228a1bd
Naren <lnaren532@gmail.com>
2025-06-30 17:49:58 +0530

    update

--- for_each_loops/r53.tf
+++ for_each_loops/r53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "www" {
-  for_each = aws_instance.roboshop
+  for_each = aws_instance.roboshop  #forloop
   zone_id = var.zone_id
   name    = "${each.key}.${var.domain_name}" #mongodb.narendaws-84s.site
   type    = "A"
